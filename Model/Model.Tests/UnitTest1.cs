6a1057c93ecada1c667fc206fb1f148521588dc5
waterman21 <seb21-l@hotmail.be>
2016-09-29 17:43:28 +0200

    ajout du WPFClient

--- Model/Model.Tests/UnitTest1.cs
+++ Model/Model.Tests/UnitTest1.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.Entity;
+using System.Linq;
 
 namespace Model.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
+       /* [TestMethod]
         public void TestMethod1()
         {
         }
-
+        */
         [TestInitialize]
         public void Setup()
         {
@@ -23,12 +24,17 @@ namespace Model.Tests
         }
 
         [TestMethod]
-        public void CanGetCustomers()
+        public void InsertionFonctionnelle()
         {
             using (var context = GetContext())
             {
                 Assert.AreEqual(1, context.Customers.ToList().Count);
             }
+        }
+
+        public CompanyContext GetContext()
+        {
+            return new CompanyContext();
         }
     }
 }
