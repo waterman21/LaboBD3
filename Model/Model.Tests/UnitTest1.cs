b609214b58ed6cef2ec09406848bfe51c1fd1df4
waterman21 <seb21-l@hotmail.be>
2016-10-06 16:58:22 +0200

    test concurrent

--- Model/Model.Tests/UnitTest1.cs
+++ Model/Model.Tests/UnitTest1.cs
@@ -2,17 +2,18 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Data.Entity;
 using System.Linq;
+using System.Data.Entity.Infrastructure;
 
 namespace Model.Tests
 {
     [TestClass]
     public class UnitTest1
     {
-       /* [TestMethod]
-        public void TestMethod1()
-        {
-        }
-        */
+        /* [TestMethod]
+         public void TestMethod1()
+         {
+         }
+         */
         [TestInitialize]
         public void Setup()
         {
@@ -35,6 +36,27 @@ namespace Model.Tests
         public CompanyContext GetContext()
         {
             return new CompanyContext();
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(DbUpdateConcurrencyException))]
+        public void DetecterLesEditionsConcurrentes()
+        {
+            using (CompanyContext context1 = GetContext())
+            {
+                using (CompanyContext context2 = GetContext())
+                {
+                    var client1 = context1.Customers.First();
+                    var client2 = context2.Customers.First();
+
+                    client1.AccountBalance += 1000;
+                    context1.SaveChanges();
+
+                    client2.AccountBalance += 1000;
+                    context2.SaveChanges();
+                }
+            }
         }
     }
 }
